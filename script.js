14d18b8f7021b6fd53fcad8641fd8d528711ba6e
Batrisyia Amani <amanihaled@gmail.com>
2026-06-11 15:19:16 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.querySelector('.navbar');
+    
+    // Smooth Scrolling tracking behavior
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            if(target) {
+                target.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
