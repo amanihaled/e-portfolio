de1795a0ec3bc1b6b9b72669674125a1e34a1e5e
Batrisyia Amani <amanihaled@gmail.com>
2026-06-11 15:18:53 +0800

    Create projects.php

---
+++ projects.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projects | Batrisyia Amani</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="logo">amanihaled</div>
+            <ul class="nav-links">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="projects.php">Projects</a></li>
+                <li><a href="index.php#contact" class="btn-contact">Get in touch!</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="main-content-wrapper" style="background-color: var(--white); min-height: 85vh; border-radius: 0;">
+        <h2 class="section-title">Portfolio Projects</h2>
+        <div class="project-flex-container">
+            <article class="project-card">
+                <h3>Project 1: Early Development of Personal Portfolio</h3>
+                <p>A responsive website built using only HTML and CSS Grid.</p>
+                <a href="https://activity-2-lab-test.vercel.app/" target="_blank">View Draft 1</a>
+            </article>
+
+            <article class="project-card">
+                <h3>Project 2: Second Development of Personal Portfolio</h3>
+                <p>A responsive website built using CSS Grid.</p>
+                <a href="https://my-flexbox-six.vercel.app/" target="_blank">View Draft 2</a>    
+            </article>
+        </div>
+    </main>
+
+    <footer>
+        <p>© 2026 Batrisyia Amani | SECV2223 Web Programming</p>
+    </footer>
+</body>
+</html>
